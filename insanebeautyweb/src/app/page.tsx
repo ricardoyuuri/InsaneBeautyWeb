6633792771e7507470f7f2f2b49c6a798aeaa2e7
Ricardo <domingues.yuri13@gmail.com>
2024-04-09 16:20:04 -0300

    atualizando

--- insanebeautyweb/src/app/page.tsx
+++ insanebeautyweb/src/app/page.tsx
@@ -1,9 +1,16 @@
+import NavBar from "@/componentes/NavBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+       <NavBar active="agendamento" />
+
+    <section className="flex flex-col gap-2 bg-slate-800 min-w-[600px] mt-4 p-2 rounded">
+    <div className="flex justify-between">
+    <h2 className="text-2xl font-bold">Agendamento</h2>
+    </div>
+</section>
     </main>
   );
 }
